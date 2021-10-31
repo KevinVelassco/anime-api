a8dcc46dcd9b9d5832f7c29f6fd419122036e89c
KevinVelassco <kevinvelassco@gmail.com>
2021-10-31 13:47:28 -0500

    feat: animes entity is created

--- src/modules/anime/anime.entity.ts
+++ src/modules/anime/anime.entity.ts
@@ -1,12 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  Unique
+} from 'typeorm';
 
+@Entity('animes')
+@Unique('uq_uid', ['uid'])
+@Unique('uq_name', ['name'])
 export class Anime {
   @ApiProperty()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @ApiProperty()
+  @Column()
+  @Generated('uuid')
   uid: string;
 
   @ApiProperty()
+  @Column({ type: 'varchar', length: 50 })
   name: string;
 }
