8cfe21aae1b8d1e38ac6c20031478bd9ac149213
KevinVelassco <kevinvelassco@gmail.com>
2021-11-25 20:14:17 -0500

    feat: users service is created

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserInput } from './dtos/create-user-input.dto';
+import { GetUserByEmailInput } from './dtos/get-user-by-email-input.dto';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  public async create(createUserInput: CreateUserInput): Promise<User> {
+    const { email } = createUserInput;
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email
+      }
+    });
+
+    if (user) {
+      throw new NotFoundException(`already exists a user with email ${email}`);
+    }
+
+    const created = this.userRepository.create({
+      ...createUserInput
+    });
+
+    created.hashPassword();
+
+    const saved = await this.userRepository.save(created);
+
+    return saved;
+  }
+
+  public async getUserByEmail(
+    getUserByEmailInput: GetUserByEmailInput
+  ): Promise<User | null> {
+    const { email } = getUserByEmailInput;
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email
+      }
+    });
+
+    return user || null;
+  }
+}
