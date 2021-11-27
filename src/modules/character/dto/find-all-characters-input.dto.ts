5bcc11d6ab2135be6c8e5bcd5bc4611d9f6ef97e
KevinVelassco <kevinvelassco@gmail.com>
2021-11-26 22:10:34 -0500

    feat: is created dto (find all characters)

---
+++ src/modules/character/dto/find-all-characters-input.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString
+} from 'class-validator';
+
+import { CharacterGender, CharacterStatus } from '../character.entity';
+
+export class FindAllCharactersInput {
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  readonly limit?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  readonly skip?: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly name?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsEnum(CharacterStatus)
+  readonly status?: CharacterStatus;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly species?: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsEnum(CharacterGender)
+  readonly gender?: CharacterGender;
+}
