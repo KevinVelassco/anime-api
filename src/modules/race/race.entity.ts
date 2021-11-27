d8aa5da4b382ad4de216d48158342a2805d73381
KevinVelassco <kevinvelassco@gmail.com>
2021-11-27 14:43:54 -0500

    feat: race entity is created

---
+++ src/modules/race/race.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn
+} from 'typeorm';
+
+import { ApiProperty } from '@nestjs/swagger';
+import { Exclude } from 'class-transformer';
+import { Character } from '../character/character.entity';
+
+@Entity('reces')
+@Unique('uq_race_uid', ['uid'])
+@Unique('uq_race_name', ['name'])
+export class Race {
+  @Exclude()
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  @Generated('uuid')
+  uid: string;
+
+  @ApiProperty()
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Exclude()
+  @ApiProperty()
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Exclude()
+  @ApiProperty()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @OneToMany(() => Character, (character: Character) => character.race)
+  characters: Character[];
+}
