4a22d091582e2a1b48f1daa6226f98635bff69df
KevinVelassco <kevinvelassco@gmail.com>
2021-11-26 22:22:26 -0500

    feat: the route to find all the characters is added to the controller

--- src/modules/character/character.controller.ts
+++ src/modules/character/character.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { Public } from '../../common/decorators/public.decorator';
+import { CharacterService } from './character.service';
+import { FindAllOutPut } from '../../common/dto/find-all-output.dto';
+import { FindAllCharactersInput } from './dto/find-all-characters-input.dto';
+
+@ApiTags('character')
 @Controller('character')
-export class CharacterController {}
+export class CharacterController {
+  constructor(private readonly characterService: CharacterService) {}
+
+  @Public()
+  @Get()
+  findAll(
+    @Query() findAllCharactersInput: FindAllCharactersInput
+  ): Promise<FindAllOutPut> {
+    return this.characterService.findAll(findAllCharactersInput);
+  }
+}
--- src/modules/character/character.module.ts
+++ src/modules/character/character.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CharacterController } from './character.controller';
+import { Character } from './character.entity';
 import { CharacterService } from './character.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Character])],
   controllers: [CharacterController],
   providers: [CharacterService]
 })
