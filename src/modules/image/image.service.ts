b2c294918836864e1d232935e5d4a989121cf533
KevinVelassco <kevinvelassco@gmail.com>
2021-11-28 15:26:14 -0500

    feat: method to create image is added

--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Image } from './image.entity';
 import { FindAllImagesInput } from './dto/find-all-images-input.dto';
 import { FindOneImageInput } from './dto/find-one-image-input.dto';
+import { CreateImageInput } from './dto/create-image-input.dto';
 
 @Injectable()
 export class ImageService {
@@ -39,5 +44,22 @@ export class ImageService {
     }
 
     return item || null;
+  }
+
+  public async create(createImageInput: CreateImageInput): Promise<Image> {
+    const { url } = createImageInput;
+
+    const existingByUrl = await this.imageRepository
+      .createQueryBuilder('i')
+      .where('i.url = :url', { url })
+      .getOne();
+
+    if (existingByUrl) {
+      throw new ConflictException(`already exists a image with url ${url}`);
+    }
+
+    const created = this.imageRepository.create(createImageInput);
+    const saved = await this.imageRepository.save(created);
+    return saved;
   }
 }
