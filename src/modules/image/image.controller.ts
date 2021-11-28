7e1c3ddae94e3df89b1949052baa24622b284559
KevinVelassco <kevinvelassco@gmail.com>
2021-11-28 14:57:20 -0500

    feat: the route to find one image is added to the controller

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { ImageService } from './image.service';
+import { Image } from './image.entity';
 import { Public } from '../../common/decorators/public.decorator';
 import { FindAllImagesInput } from './dto/find-all-images-input.dto';
+import { FindOneImageInput } from './dto/find-one-image-input.dto';
 
 @ApiTags('image')
 @Controller('image')
@@ -14,5 +16,13 @@ export class ImageController {
   @Get()
   findAll(@Query() findAllImagesInput: FindAllImagesInput): Promise<any> {
     return this.imageService.findAll(findAllImagesInput);
+  }
+
+  @Public()
+  @Get(':uid')
+  findOne(
+    @Param() findOneImageInput: FindOneImageInput
+  ): Promise<Image | null> {
+    return this.imageService.findOne(findOneImageInput);
   }
 }
