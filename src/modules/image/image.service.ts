baf55799efbd9e9c2652816c00ecad0f36ab0aea
KevinVelassco <kevinvelassco@gmail.com>
2021-11-28 14:55:15 -0500

    feat: method to find one image is added

--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Image } from './image.entity';
 import { FindAllImagesInput } from './dto/find-all-images-input.dto';
+import { FindOneImageInput } from './dto/find-one-image-input.dto';
 
 @Injectable()
 export class ImageService {
@@ -24,5 +25,19 @@ export class ImageService {
     });
 
     return items;
+  }
+
+  public async findOne(
+    findOneImageInput: FindOneImageInput
+  ): Promise<Image | null> {
+    const { uid, checkIfExists = false } = findOneImageInput;
+
+    const item = await this.imageRepository.findOne({ where: { uid } });
+
+    if (checkIfExists && !item) {
+      throw new NotFoundException(`can't get the image with uuid ${uid}.`);
+    }
+
+    return item || null;
   }
 }
