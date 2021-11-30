8067498b2dc90ffa118c9bd511a095c27300473e
KevinVelassco <kevinvelassco@gmail.com>
2021-11-30 00:04:00 -0500

    feat: the route to remove assigned image from character is added to the controller

--- src/modules/assigned-image/assigned-image.controller.ts
+++ src/modules/assigned-image/assigned-image.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { AssignedImage } from './assigned-image.entity';
 import { AssignedImageService } from './assigned-image.service';
 import { AssignImageToCharacterInput } from './dto/assign-image-to-character-input.dto';
+import { RemoveAssignedImageFromCharacterInput } from './dto/remove-assigned-image-from-character-input.dto';
 
 @ApiTags('assigned-image')
 @Controller('assigned-image')
@@ -11,11 +12,21 @@ export class AssignedImageController {
   constructor(private readonly assignedImageService: AssignedImageService) {}
 
   @Post()
-  create(
+  assignImageToCharacter(
     @Body() assignImageToCharacterInput: AssignImageToCharacterInput
   ): Promise<AssignedImage> {
     return this.assignedImageService.assignImageToCharacter(
       assignImageToCharacterInput
+    );
+  }
+
+  @Delete()
+  removeAssignedImageFromCharacter(
+    @Body()
+    removeAssignedImageFromCharacterInput: RemoveAssignedImageFromCharacterInput
+  ): Promise<AssignedImage> {
+    return this.assignedImageService.removeAssignedImageFromCharacter(
+      removeAssignedImageFromCharacterInput
     );
   }
 }
