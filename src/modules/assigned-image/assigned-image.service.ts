d6af3ece6172485c1339d6815db9f5ec978eae18
KevinVelassco <kevinvelassco@gmail.com>
2021-11-30 00:02:16 -0500

    feat: added method of remove assigned image from character

--- src/modules/assigned-image/assigned-image.service.ts
+++ src/modules/assigned-image/assigned-image.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -6,6 +10,7 @@ import { CharacterService } from '../character/character.service';
 import { ImageService } from '../image/image.service';
 import { AssignedImage } from './assigned-image.entity';
 import { AssignImageToCharacterInput } from './dto/assign-image-to-character-input.dto';
+import { RemoveAssignedImageFromCharacterInput } from './dto/remove-assigned-image-from-character-input.dto';
 
 @Injectable()
 export class AssignedImageService {
@@ -45,11 +50,44 @@ export class AssignedImageService {
 
     if (existing) {
       throw new ConflictException(
-        `the image is already assigned to the character`
+        'the image is already assigned to the character'
       );
     }
 
     const saved = await this.assignedImageRepository.save(created);
+
+    return saved;
+  }
+
+  public async removeAssignedImageFromCharacter(
+    removeAssignedImageFromCharacterInput: RemoveAssignedImageFromCharacterInput
+  ): Promise<AssignedImage> {
+    const { characterUid } = removeAssignedImageFromCharacterInput;
+
+    const character = await this.characterService.findOne({
+      uid: characterUid,
+      checkIfExists: true
+    });
+
+    const { imageUid } = removeAssignedImageFromCharacterInput;
+
+    const image = await this.imageService.findOne({
+      uid: imageUid,
+      checkIfExists: true
+    });
+
+    const existing = await this.assignedImageRepository.findOne({
+      character,
+      image
+    });
+
+    if (!existing) {
+      throw new NotFoundException(
+        `the image is not assigned to the character with uuid ${character.uid}.`
+      );
+    }
+
+    const saved = await this.assignedImageRepository.remove(existing);
 
     return saved;
   }
