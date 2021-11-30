9b8621b8fcd6f9e3527e33f34edc0e1d8d36b414
KevinVelassco <kevinvelassco@gmail.com>
2021-11-29 23:07:27 -0500

    feat: is created dto (assign image to character)

---
+++ src/modules/assigned-image/dto/assign-image-to-character-input.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class AssignImageToCharacterInput {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  readonly characterUid: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  @IsUUID()
+  readonly imageUid: string;
+}
