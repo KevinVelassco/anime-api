4201db8f4a9347068d11fa9a7018f0e05426a845
KevinVelassco <kevinvelassco@gmail.com>
2021-12-06 20:31:34 -0500

    feat: is created dto (get image by uid)

---
+++ src/modules/image/dto/get-image-by-uid-input.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class GetImageByUidInput {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  readonly uid: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsBoolean()
+  checkIfExists?: boolean;
+}
