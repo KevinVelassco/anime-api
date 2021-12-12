792bbbe7141571eb5c5d8f728a0e5d90e15d55fd
KevinVelassco <kevinvelassco@gmail.com>
2021-12-12 13:54:43 -0500

    feat: the route to find all origins is added to the controller

--- src/modules/origin/origin.controller.ts
+++ src/modules/origin/origin.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { Origin } from './origin.entity';
 import { OriginService } from './origin.service';
 import { Public } from '../../common/decorators/public.decorator';
 import { FindOneOriginInput } from './dto/find-one-origin-input.dto';
+import { FindAllOriginsInput } from './dto/find-all-origins-input.dto';
 
 @ApiTags('origin')
 @Controller('origin')
 export class OriginController {
   constructor(private readonly originService: OriginService) {}
+
+  @Public()
+  @Get()
+  findAll(@Query() findAllOriginsInput: FindAllOriginsInput): Promise<any> {
+    return this.originService.findAll(findAllOriginsInput);
+  }
 
   @Public()
   @Get(':uid')
