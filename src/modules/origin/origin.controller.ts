d2b9b9c48dc648de736295251c40d9dc2e6176f1
KevinVelassco <kevinvelassco@gmail.com>
2021-12-12 15:58:46 -0500

    feat: the route to create origin is added to the controller

--- src/modules/origin/origin.controller.ts
+++ src/modules/origin/origin.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { Origin } from './origin.entity';
 import { OriginService } from './origin.service';
 import { Public } from '../../common/decorators/public.decorator';
 import { FindOneOriginInput } from './dto/find-one-origin-input.dto';
 import { FindAllOriginsInput } from './dto/find-all-origins-input.dto';
+import { UploadFile } from '../../common/interfaces/upload-file.interface';
+import { CreateOriginInput } from './dto/create-origin-input-dto';
 
 @ApiTags('origin')
 @Controller('origin')
@@ -24,5 +36,14 @@ export class OriginController {
     @Param() findOneOriginInput: FindOneOriginInput
   ): Promise<Origin | null> {
     return this.originService.findOne(findOneOriginInput);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  create(
+    @Body() createOriginInput: CreateOriginInput,
+    @UploadedFile() file: UploadFile
+  ): Promise<Origin> {
+    return this.originService.create(createOriginInput, file);
   }
 }
