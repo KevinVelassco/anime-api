f76e9c886bc9b562c06ba66652858c2d34b86067
KevinVelassco <kevinvelassco@gmail.com>
2021-12-12 13:53:20 -0500

    feat: method is created to find all origins

--- src/modules/origin/origin.service.ts
+++ src/modules/origin/origin.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 
 import { Origin } from './origin.entity';
 import { FindOneOriginInput } from './dto/find-one-origin-input.dto';
+import { FindAllOriginsInput } from './dto/find-all-origins-input.dto';
 
 @Injectable()
 export class OriginService {
@@ -11,6 +12,25 @@ export class OriginService {
     @InjectRepository(Origin)
     private readonly originRepository: Repository<Origin>
   ) {}
+
+  public async findAll(findAllOriginsInput: FindAllOriginsInput): Promise<any> {
+    const { limit = 10, skip = 0, q } = findAllOriginsInput;
+
+    let where: any;
+
+    if (q) where = { name: ILike(`%${q}%`) };
+
+    const items = await this.originRepository.findAndCount({
+      where,
+      take: limit,
+      skip,
+      order: {
+        id: 'DESC'
+      }
+    });
+
+    return items;
+  }
 
   public async findOne(
     findOneOriginInput: FindOneOriginInput
