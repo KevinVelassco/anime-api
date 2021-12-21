4902f68b7ea598e90e3f57bec6ea5292551e3f14
KevinVelassco <kevinvelassco@gmail.com>
2021-12-21 14:18:46 -0500

    feat: uthorization guardian added globally

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { AuthorizationGuard } from './guards/authorization.guard';
 
 @Module({
   providers: [
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthorizationGuard
     }
   ]
 })
