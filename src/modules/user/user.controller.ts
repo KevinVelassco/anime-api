29565501dae580d1ef78a171a84c0f1dc81339dd
KevinVelassco <kevinvelassco@gmail.com>
2021-12-21 17:08:44 -0500

    feat: the route to find a user is added to the controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { CreateUserInput } from './dto/create-user-input.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { Admin } from '../../common/decorators/admin.decorator';
+import { FindOneUserInput } from './dto/find-one-user-input.dto';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Admin()
+  @Get(':uid')
+  findOne(@Param() findOneUserInput: FindOneUserInput): Promise<User | null> {
+    return this.userService.findOne(findOneUserInput);
+  }
 
   @Admin()
   @Post()
