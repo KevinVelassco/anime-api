b16095f1a5ab32794f2ef9aa49fb54d30831343b
KevinVelassco <kevinvelassco@gmail.com>
2022-01-10 12:15:01 -0500

    feat: the route to generate tokens is added to the controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,8 +3,10 @@ import { Request } from 'express';
 
 import { Public } from '../../common/decorators/public.decorator';
 import { LocalAuthGuard } from '../../common/guards/local-auth.guard';
+import { JwtRefreshAuthGuard } from '../../common/guards/jwt-refresh.guard';
 import { User } from '../user/user.entity';
 import { AuthService } from './auth.service';
+import { Tokens } from './types/tokens.type';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +18,13 @@ export class AuthController {
   login(@Req() req: Request) {
     const user = req.user as User;
     return this.authService.login(user);
+  }
+
+  @Public()
+  @UseGuards(JwtRefreshAuthGuard)
+  @Post('refresh')
+  refreshTokens(@Req() req: Request): Tokens {
+    const user = req.user as User;
+    return this.authService.refreshTokens(user);
   }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,32 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
-import appConfig from './../../config/app.config';
 import { UserModule } from '../user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './stratigies/local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './stratigies/jwt.strategy';
+import { JwtRefreshStrategy } from './stratigies/jwt-refresh.strategy';
 
 @Module({
-  imports: [
-    PassportModule,
-    UserModule,
-    JwtModule.registerAsync({
-      inject: [appConfig.KEY],
-      useFactory: (configService: ConfigType<typeof appConfig>) => {
-        return {
-          secret: configService.app.accessTokenSecret,
-          signOptions: {
-            expiresIn: '1d'
-          }
-        };
-      }
-    })
-  ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [PassportModule, UserModule, JwtModule.register({})],
+  providers: [AuthService, LocalStrategy, JwtStrategy, JwtRefreshStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
