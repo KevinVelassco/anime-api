9c2d582435349305f72a647b8805f90c67ff51ac
KevinVelassco <kevinvelassco@gmail.com>
2022-01-12 21:19:56 -0500

    feat: method is created to change the authentication password of a user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,18 +1,23 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 import appConfig from '../../config/app.config';
 import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
 import { JwtPayload } from './types/jwt-payload.type';
 import { Tokens } from './types/tokens.type';
+import { ChangeAuthPasswordInput } from './dto/change-auth-password-input.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(appConfig.KEY)
     private readonly appConfiguration: ConfigType<typeof appConfig>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
     private readonly userService: UserService,
     private jwtService: JwtService
   ) {}
@@ -55,5 +60,49 @@ export class AuthService {
       access_token: accessToken,
       refresh_token: refreshToken
     };
+  }
+
+  public async changePassword(
+    user: User,
+    changeAuthPasswordInput: ChangeAuthPasswordInput
+  ): Promise<Object> {
+    const { authUid, email } = changeAuthPasswordInput;
+
+    if (authUid !== user.uid) throw new ConflictException('invalid authUid.');
+
+    if (email !== user.email) throw new ConflictException('invalid email.');
+
+    const existing = await this.userService.getUserByAuthUidAndEmail({
+      authUid,
+      email,
+      checkIfExists: true
+    });
+
+    const { oldPassword } = changeAuthPasswordInput;
+
+    const isValidPassword = await existing.checkPassword(oldPassword);
+
+    if (!isValidPassword) {
+      throw new ConflictException('old password is invalid.');
+    }
+
+    const { newPassword } = changeAuthPasswordInput;
+
+    const isSameOldPassword = await existing.checkPassword(newPassword);
+
+    if (isSameOldPassword) {
+      throw new ConflictException(
+        'the new password must be different from the old one.'
+      );
+    }
+
+    const preloaded = await this.userRepository.preload({
+      id: existing.id,
+      password: newPassword
+    });
+
+    await this.userRepository.save(preloaded);
+
+    return { message: 'password changed successfully.' };
   }
 }
