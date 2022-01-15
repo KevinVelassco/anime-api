94efb1ce2563cfc8ac2a7aeff678ab6423e6d1d7
KevinVelassco <kevinvelassco@gmail.com>
2022-01-14 21:23:36 -0500

    feat: mailgun module is created

---
+++ src/common/plugins/mailgun/mailgun.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { MailgunService } from './mailgun.service';
+import appConfig from '../../../config/app.config';
+
+@Module({
+  imports: [ConfigModule.forFeature(appConfig)],
+  providers: [MailgunService],
+  exports: [MailgunService]
+})
+export class MailgunModule {}
