3daff4e6699b150de0def6200350def6972108cf
KevinVelassco <kevinvelassco@gmail.com>
2022-01-15 15:05:27 -0500

    feat: method responsible for validating verification codes is created

--- src/modules/verification-code/verification-code.service.ts
+++ src/modules/verification-code/verification-code.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { generateUuid } from '../../utils';
 import { VerificationCode } from './verification-code.entity';
 import { CreateVerificationCodeInput } from './dto/create-verification-code-input.dto';
+import { ValidateVerificationCodeInput } from './dto/validate-verification-code-input.dto';
 
 @Injectable()
 export class VerificationCodeService {
@@ -24,5 +29,42 @@ export class VerificationCodeService {
     const saved = await this.verificationCodeRepository.save(created);
 
     return saved;
+  }
+
+  public async validate(
+    validateVerificationCodeInput: ValidateVerificationCodeInput
+  ): Promise<VerificationCode> {
+    const { code } = validateVerificationCodeInput;
+
+    const existing = await this.verificationCodeRepository.findOne({
+      where: {
+        code
+      },
+      relations: ['user']
+    });
+
+    if (!existing) {
+      throw new NotFoundException(
+        `the code ${code} is invalid, probably because it was already used.`
+      );
+    }
+
+    const currentDate = new Date();
+
+    const { expirationDate } = existing;
+
+    if (currentDate.getTime() > expirationDate.getTime()) {
+      throw new ConflictException(`the verification code ${code} is expired.`);
+    }
+
+    const { type } = validateVerificationCodeInput;
+
+    if (type !== existing.type) {
+      throw new ConflictException(
+        `the verification code ${code} doesn't have the expected type.`
+      );
+    }
+
+    return existing;
   }
 }
