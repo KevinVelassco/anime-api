3236c350b8e8c5a9a2d633d470e1e08c909b4646
KevinVelassco <kevinvelassco@gmail.com>
2022-01-15 13:46:49 -0500

    feat: add time to date function is added

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,29 @@
 import { nanoid } from 'nanoid';
 
 export const generateUuid = (size?: number): string => nanoid(size);
+
+export enum TimeType {
+  Days = 'd',
+  Hours = 'h',
+  Minutes = 'm'
+}
+
+export const addTimeToDate = (
+  date: Date,
+  time: number,
+  type: TimeType
+): Date => {
+  const dayInHours = 24;
+  const hourinMinutes = 60;
+  const minuteInMillisecond = 60000;
+
+  const timeSettingAccordingType = {
+    d: dayInHours * hourinMinutes * minuteInMillisecond,
+    h: hourinMinutes * minuteInMillisecond,
+    m: minuteInMillisecond
+  };
+
+  const extraTime = time * timeSettingAccordingType[type];
+
+  return new Date(date.getTime() + extraTime);
+};
